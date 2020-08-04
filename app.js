d2c1fa471184303057e68a520fa10d7e52fdec43
William Joel Clavell <wjclavell28@gmail.com>
2020-08-04 12:04:34 -0400

    changed add review icon, created new modal for adding review

--- app.js
+++ app.js
@@ -105,18 +105,9 @@ const showOne = async () => {
     `url(${data.boxArt})`
   );
   //append the title and add symbol to the game
-  $game.append(`<h4 class="title" id="showgame">${data.title}</h4><svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 16 16"
-    class="bi bi-plus-circle-fill"
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg">
-    <path
-      fill-rule="evenodd"
-      d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z"
-    />
-  </svg>`);
+  $game.append(
+    `<h4 class="title" id="showgame">${data.title}</h4><i class="fas fa-plus-circle" id="${data._id}"></i>`
+  );
   //append it to the section
   $onegame.append($game);
   //append game description
